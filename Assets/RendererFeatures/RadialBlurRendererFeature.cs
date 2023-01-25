8448ee7ed75cdee7e66b200359e6035e6ed348ce
Myeongseon Kim <myeongseon.kim@unity3d.com>
2023-01-25 23:21:08 +0900

    Add Radial Blur

---
+++ Assets/RendererFeatures/RadialBlurRendererFeature.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class RadialBlurRendererFeature : ScriptableRendererFeature
+{
+    public Material passMaterial;
+    private RadialBlurPass m_RadialBlurPass;
+    public RenderPassEvent injectionPoint = RenderPassEvent.AfterRenderingPostProcessing;
+    public ScriptableRenderPassInput requirements = ScriptableRenderPassInput.Color;
+
+    public override void Create()
+    {
+        m_RadialBlurPass = new RadialBlurPass();
+        m_RadialBlurPass.renderPassEvent = injectionPoint;
+        m_RadialBlurPass.ConfigureInput(requirements);
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (passMaterial == null)
+        {
+            Debug.LogWarningFormat("Missing Radiar Blur effect Material");
+            return;
+        }
+        m_RadialBlurPass.Setup(passMaterial, "RadirBlurPassRendererFeature", renderingData);
+        renderer.EnqueuePass(m_RadialBlurPass);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        m_RadialBlurPass.Dispose();
+    }
+
+
+    class RadialBlurPass : ScriptableRenderPass
+    {
+        private Material m_PassMaterial;
+        private PassData m_PassData;
+        private ProfilingSampler m_ProfilingSampler;
+        private RTHandle m_CopiedColor;
+        private static readonly int m_BlitTextureShaderID = Shader.PropertyToID("_BlitTexture");
+
+
+        public void Setup(Material mat, string featureName, in RenderingData renderingData)
+        {
+            m_PassMaterial = mat;
+            m_ProfilingSampler ??= new ProfilingSampler(featureName);
+            var colorCopyDescriptor = renderingData.cameraData.cameraTargetDescriptor;
+            colorCopyDescriptor.depthBufferBits = (int) DepthBits.None;
+            RenderingUtils.ReAllocateIfNeeded(ref m_CopiedColor, colorCopyDescriptor, name: "_RadiarBlurPassColorCopy");
+            m_PassData ??= new PassData();
+        }
+
+        public void Dispose()
+        {
+            m_CopiedColor?.Release();
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            m_PassData.effectMaterial = m_PassMaterial;
+            m_PassData.profilingSampler = m_ProfilingSampler;
+            m_PassData.copiedColor = m_CopiedColor;
+
+            ExecutePass(m_PassData, ref renderingData, ref context);
+        }
+
+        private static void ExecutePass(PassData passData, ref RenderingData renderingData, ref ScriptableRenderContext context)
+        {
+            if (passData.effectMaterial == null)
+                return;
+
+            if (renderingData.cameraData.isPreviewCamera)
+                return;
+
+            var cmd = CommandBufferPool.Get();
+            var cameraData = renderingData.cameraData;
+
+            // using (new ProfilingScope(cmd, passData.profilingSampler))
+            {
+                var source = cameraData.renderer.cameraColorTargetHandle;
+                Blitter.BlitCameraTexture(cmd, source, passData.copiedColor);
+                passData.effectMaterial.SetTexture(m_BlitTextureShaderID, passData.copiedColor);
+
+                CoreUtils.SetRenderTarget(cmd, cameraData.renderer.cameraColorTargetHandle);
+                CoreUtils.DrawFullScreen(cmd, passData.effectMaterial);
+                context.ExecuteCommandBuffer(cmd);
+                cmd.Clear();
+            }
+        }
+
+
+        private class PassData
+        {
+            internal Material effectMaterial;
+            public ProfilingSampler profilingSampler;
+            public RTHandle copiedColor;
+        }
+    }
+}
---
+++ Assets/Shaders/RadialBlur.shader
@@ -0,0 +1,80 @@
+Shader "Custom/RadialBlur"
+{
+    Properties
+    {
+        _SampleCount("SampleCount", Float) = 16
+        _Intensity("Intensity", Range(0.0, 1.0)) = 0.5
+    }
+    
+    SubShader
+    {
+        Tags {"RenderType" = "Opaque"  "RenderPipeline" = "UniversalPipeline"}
+        LOD 100
+
+        Pass
+        {
+            Name "RadialBlur"
+            ZTest Always
+            ZWrite Off
+            Cull Off
+
+            HLSLPROGRAM
+            #pragma vertex RadialBlurVert
+            #pragma fragment RadialBlurFragment
+
+                
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "Packages/com.unity.render-pipelines.core/Runtime/Utilities/Blit.hlsl"
+
+            SAMPLER(sampler_BlitTexture);
+
+            float _SampleCount;
+            float _Intensity;
+
+            Varyings RadialBlurVert(Attributes input)
+            {
+                Varyings output;
+                UNITY_SETUP_INSTANCE_ID(input);
+                UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+
+            #if SHADER_API_GLES
+                float4 pos = input.positionOS;
+                float2 uv  = input.uv;
+            #else
+                float4 pos = GetFullScreenTriangleVertexPosition(input.vertexID);
+                float2 uv  = GetFullScreenTriangleTexCoord(input.vertexID);
+            #endif
+
+                output.positionCS = pos;
+                output.texcoord   = uv;
+                return output;
+            }
+
+            half4 RadialBlurFragment(Varyings input) : SV_Target
+            {
+                UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+                float2 uv = UnityStereoTransformScreenSpaceTex(input.texcoord);
+                half3 color = SAMPLE_TEXTURE2D_X(_BlitTexture, sampler_BlitTexture, uv).xyz;
+                half3 finalColor = color;
+
+                float2 v = float2(0.5, 0.5) - uv;
+                half dist = length(v);
+                float2 dir = normalize(v);
+                float2 offset = dir * rcp(_SampleCount) * _Intensity * dist;
+
+
+                for (int idx = 1; idx <= _SampleCount; ++idx)
+                {
+                    finalColor += SAMPLE_TEXTURE2D_X(_BlitTexture, sampler_BlitTexture, uv).xyz;
+                    uv += offset;
+                }
+                finalColor /= _SampleCount;
+
+                return half4(finalColor, 1);
+            }
+
+
+            ENDHLSL
+        }
+    }
+}
